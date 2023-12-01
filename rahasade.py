de4e629f3b714a8f41159f8d8252787bf2b1088f
Lalefal <94318146+Lalefal@users.noreply.github.com>
2023-12-01 12:56:45 +0200

    Add files via upload
    Lisää ominaisuuksia

--- rahasade.py
+++ rahasade.py
@@ -1,13 +1,15 @@
 
 """
-
     Rahasade
 
     Ruudun alareunassa on robotti, jota pelaaja voi liikuttaa vasemmalle tai oikealle
     Taivaalta sataa rahaa, jota robotin tulee kerätä.
+    Jos kolikko pääsee pelin alareunaan, elämät vähenee yhdellä.
     Taivaalta sataa myös hirviöitä, joita robotin tulee väistellä.
-    Osuma hirviöön vähentää elämiä. 
-    Jos elämät loppuvat tai kolikko pääsee pelin alareunaan, peli loppuu.
+    Osuma hirviöön vähentää yhden elämän. 
+    Jos elämät loppuvat peli loppuu.
+    50 kolikkoa == 1 elämä lisää
+    Peli pitää kirjaa parhaasta pistetuloksesta
     Versio2 someday:
     Hirviöitä voi myös ampua kerätyillä kolikoilla, jolloin kolikkosaldo vähenee
     ? Kun hirviöön osuus, siitä sinkoaa kolikoita, joita kerätä
@@ -27,45 +29,56 @@ class Kolikkosade:
         ohjefontti = pygame.font.SysFont("Arial", 16)
         self.ohjeteksti = ohjefontti.render("Enter = tauko  F2 = uusi peli  Esc = sulje peli ", True, (255, 0, 0)) 
         self.ohjeen_koko = self.ohjeteksti.get_height() + 10
+        self.ennatys = 0
+        self.uusi_ennatys = self.ennatys
         
         self.uusi_peli()
 
-    def uusi_peli(self):        
+    def uusi_peli(self):      
+        """Alustaa uuden pelin"""  
         self.luo_kolikot()
         self.luo_hirviot()
         self.luo_robo()
         self.pelaa()
+
         
     def luo_kolikot(self):
+        """Luodaan pelissa nakyvat kolikot"""
         self.kolikot = []        
         self.kolikot_kuva = pygame.image.load("kolikko.png")        
-        self.maks_kolikot = 10
+        self.maks_kolikot = 10 # Maksimimäärä näkyviä kolikoita
         self.kolikot_lisays = 0
-        self.kolikot_odotusaika = random.randint(1000, 3000) 
-        self.missasit = False
+        self.kolikot_odotusaika = random.randint(1000, 3000) # Satunnainen alkuodotusaika millisekunteina
+        self.missasit = False #osuuko robotti kolikkoon
+        self.missasit_aika = None #Jos robotti ei osu kolikkoon -> teksti UI UI yhden sekunnin ajan
         
     def luo_hirviot(self):
+        """Luodaan pelissa nakyvat hirviot"""        
         self.hirviot = []
         self.hirvio_kuva = pygame.image.load("hirvio.png")
         self.maks_hirviot = 5
         self.hirviot_lisays = 0
         self.hirviot_odotusaika = random.randint(1000, 10000)
-        self.osuma = False
+        self.osuma = False #osuuko hirvio robottiin
         self.osuma_aika = None
         
     def luo_robo(self):
+        """Luodaan robotti"""
         self.robo = pygame.image.load("robo.png")        
         self.roboL, self.roboK = self.robo.get_width(), self.robo.get_height()
-        self.x = 0
-        self.y = self.korkeus - self.roboK 
+        self.x, self.y = 0, self.korkeus - self.roboK 
         self.oikealle = False
         self.vasemmalle = False
         self.tauko = False
-        self.pisteet = 0
+        self.pisteet = 0  
         self.elamat = 5
+                
+        if self.pisteet % 3 == 0 and self.pisteet > 0:
+            self.elamat += 1
             
             
     def pelaa(self):
+        """Pelataan"""
         while True:
             self.kasittele_tapahtumat()
             if not self.tauko:
@@ -74,6 +87,8 @@ class Kolikkosade:
 
 
     def kasittele_tapahtumat(self):
+        """Maarittaa nappaimet.
+        Toteuttaa uusi peli / lopeta peli nappainkomennot"""
         for tapahtuma in pygame.event.get():
             if tapahtuma.type == pygame.QUIT:
                 exit()
@@ -91,6 +106,7 @@ class Kolikkosade:
 
 
     def kasittele_nappaimet(self, key, key_up=False):
+        """Toteuttaa tauko/robotin liikkumisen nappainkomennot"""
         if key == pygame.K_LEFT:
             self.vasemmalle = not key_up
         elif key == pygame.K_RIGHT:
@@ -101,6 +117,7 @@ class Kolikkosade:
             
 
     def aseta_tauko_otsikko(self):
+        """Ikkunan otsikko muuttuu, jos peli on tauolla / kun peli jatkuu"""
         if self.tauko:
             pygame.display.set_caption("Kolikot tauolla")
         else:
@@ -108,6 +125,9 @@ class Kolikkosade:
 
 
     def paivita_peli(self):
+        """Lisätään kolikot/hirviot listalle uusi kolikko/hirvio, 
+        jos näkyvien kolikoiden/hirvjioiden 
+        määrä on alle maksimin ja odotusaika on kulunut"""
         aika = pygame.time.get_ticks()
         if len(self.kolikot) < self.maks_kolikot and aika - self.kolikot_lisays > self.kolikot_odotusaika:
             self.lisaa_kolikot()
@@ -123,12 +143,16 @@ class Kolikkosade:
 
 
     def lisaa_kolikot(self):
+        """Kolikon alkukoordinaattien arpominen"""
         kx = random.randint(0, self.leveys - self.kolikot_kuva.get_width())
         ky = 0 - self.kolikot_kuva.get_height()
         self.kolikot.append([kx, ky])
 
 
     def tarkista_kolikot(self):
+        """Tarkistetaan, osuuko robotti kolikkoon (+1 piste) 
+        tai saavuttaako kolikko ikkunan alareunan (-1 elama)
+        Poistetaan kolikko listalta, jotta listalle luodaan uusi kolikko"""
         for kolikko in self.kolikot[:]:
             if (kolikko[1] + self.kolikot_kuva.get_height() >= self.y and
                     ((kolikko[0] >= self.x and kolikko[0] <= self.x + self.roboL) or
@@ -136,18 +160,26 @@ class Kolikkosade:
                     (kolikko[0] <= self.x and kolikko[0] + self.kolikot_kuva.get_width() >= self.x + self.roboL))):
                 self.pisteet += 1
                 self.kolikot.remove(kolikko)
+                
+                if self.pisteet % 50 == 0:
+                    self.elamat += 1
 
             if kolikko[1] + self.kolikot_kuva.get_height() == self.korkeus: 
+                self.elamat -= 1
                 self.missasit = True
-
+                self.kolikot.remove(kolikko)
+                
 
     def lisaa_hirviot(self):
+        """Hirvioiden alkukoordinaattien arpominen"""
         hx = random.randint(0, self.leveys - self.hirvio_kuva.get_width())
         hy = 0 - self.hirvio_kuva.get_height()
         self.hirviot.append([hx, hy])
         
 
     def tarkista_hirviot(self):
+        """Tarkistetaan, osuuko robotti hirvioon (-1 elama) 
+        Poistetaan hirvio listalta, jotta listalle luodaan uusi hirvio"""        
         for hirvio in self.hirviot[:]:
             if hirvio[1] < self.korkeus - self.hirvio_kuva.get_height():  
                 if (hirvio[1] + self.hirvio_kuva.get_height() >= self.y and
@@ -162,11 +194,11 @@ class Kolikkosade:
     
     
     def liikuta_roboa(self):
+        """Robotin nopeus ja liikkumatila"""
         if self.oikealle:
             self.x += 4
         if self.vasemmalle:
             self.x -= 4
-        #robotin liikkumatila:
         self.x = max(self.x, 0)
         self.x = min(self.x, self.leveys - self.roboL)
         self.y = max(self.y, 0)
@@ -174,6 +206,7 @@ class Kolikkosade:
         
         
     def piirra_sydan(self):
+        """Sydankuva eli elamat"""
         RED = (255, 0, 0)
         points = [
             (27, 60+ self.ohjeen_koko), (9, 70 + self.ohjeen_koko), (27, 88 + self.ohjeen_koko),
@@ -185,6 +218,9 @@ class Kolikkosade:
     
 
     def piirra(self):
+        """Piirretaan asiat ikkunaan
+        Jos robotti osuu hirvioon, ikkunassa vilkkuu teksti AU AU randomeissa sijainneissa yhden sekunnin ajan.
+        Jos robotti ei saa kolikkoa kiinni, ikkunassa vilkkuu teksti UI UI randomeissa sijainneissa yhden sekunnin ajan"""
         self.naytto.fill((102, 0, 51))
         self.piirra_sydan() 
         
@@ -196,6 +232,7 @@ class Kolikkosade:
             for hirvio in self.hirviot:
                 self.naytto.blit(self.hirvio_kuva, (hirvio[0], hirvio[1] + 1))
                 hirvio[1] += 2    
+                
         
         if self.osuma:
             font = pygame.font.SysFont("Arial", 50)                
@@ -213,26 +250,57 @@ class Kolikkosade:
                 self.osuma = False  # Aseta osuma Falseksi, kun aika on kulunut loppuun
                 self.osuma_aika = None  # Nollaa aika osuman päätyttyä    
                 
+                
+        if self.missasit:
+            fontt = pygame.font.SysFont("Arial", 50)                
+            UI = fontt.render("UI UI", True, (255, 128, 0))         
+            
+            if self.missasit and self.missasit_aika is None:
+                self.missasit_aika = pygame.time.get_ticks()        
+
+            nayta_osuma = 1000   
+            if self.missasit_aika is not None and pygame.time.get_ticks() - self.missasit_aika < nayta_osuma:
+                random_x = random.randint(0, self.leveys - UI.get_width())
+                random_y = random.randint(0, self.korkeus - UI.get_height())
+                self.naytto.blit(UI, (random_x, random_y))                
+            else:
+                self.missasit = False  # Aseta osuma Falseksi, kun aika on kulunut loppuun
+                self.missasit_aika = None  # Nollaa aika osuman päätyttyä                  
+                
         if self.peli_loppui():
-            teksti = self.fontti.render("Nyyh, peli loppui!", True, (255, 0, 0))
-            self.naytto.blit(teksti, (self.naytto.get_width() / 2, self.naytto.get_height() / 2))
+            #if self.pisteet <= self.ennatys:
+            teksti1 = self.fontti.render("Nyyh, peli loppui!", True, (255, 0, 0))
+            self.naytto.blit(teksti1, (self.naytto.get_width() / 2, self.naytto.get_height() / 2))                       
+            if self.pisteet > self.ennatys:    
+                teksti2 = self.fontti.render("JEEE uusi enkka!!", True, (255, 255, 0))
+                self.naytto.blit(teksti2, (self.naytto.get_width() / 2, self.naytto.get_height() / 2 + teksti1.get_height()))
+                self.uusi_ennatys = self.pisteet
+
                 
         self.naytto.blit(self.robo, (self.x, self.y))
         
         self.naytto.blit(self.kolikot_kuva, (7, 5 + self.ohjeen_koko))
-        teksti = self.fontti.render(f" {self.pisteet} ", True, (255, 255, 0)) 
-        self.naytto.blit(teksti, (50, 11 + self.ohjeen_koko))
-        teksti = self.fontti.render(f" {self.elamat} ", True, (255, 0, 0)) 
-        self.naytto.blit(teksti, (50, 58 + self.ohjeen_koko))
+        
+        pisteet = self.fontti.render(f" {self.pisteet} ", True, (255, 255, 0)) 
+        self.naytto.blit(pisteet, (50, 11 + self.ohjeen_koko))
+        
+        elamat = self.fontti.render(f" {self.elamat} ", True, (255, 0, 0)) 
+        self.naytto.blit(elamat, (50, 58 + self.ohjeen_koko))
         self.naytto.blit(self.ohjeteksti, (7, 5)) 
+        
+        enkkafontti = pygame.font.SysFont("Arial", 20) 
+        enkka = enkkafontti.render(f"Enkka {self.uusi_ennatys} ", True, (255, 0, 0)) 
+        self.naytto.blit(enkka, (7, 69 + 2*self.ohjeen_koko)) 
         
         pygame.display.flip()
         self.kello.tick(60)    
         
 
+
     def peli_loppui(self):
-        if self.elamat == 0 or self.missasit == True:     
-           return True
+        """Seuraa pelaajan elamien maaraa ja lopettaa pelin"""
+        if self.elamat == 0:     
+            return True
 
 
 if __name__ == "__main__":
